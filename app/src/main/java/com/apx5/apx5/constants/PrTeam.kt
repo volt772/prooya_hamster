b3c4f631bcc79b39cf1e74a22e2a6c7729ed64ad
ALLENJEONG\Allen Jeong <volt772@naver.com>
2020-02-16 13:47:01 +0900

    [*] SK BI수정

--- app/src/main/java/com/apx5/apx5/constants/PrTeam.kt
+++ app/src/main/java/com/apx5/apx5/constants/PrTeam.kt
@@ -16,7 +16,7 @@ enum class PrTeam(
     DSB("dsb", "두산베어스", "두산", "#131230", "#838585", "ic_team_dsb"),
     LTG("ltg", "롯데자이언츠", "롯데", "#DD0330", "#DD0330", "ic_team_ltg"),
     NCD("ncd", "NC다이노스", "NC", "#1D467C", "#B0927C", "ic_team_ncd"),
-    SKW("skw", "SK와이번스", "SK", "#E85021", "#EF800A", "ic_team_skw"),
+    SKW("skw", "SK와이번스", "SK", "#EA002C", "#FF7A00", "ic_team_skw"),
     NXH("nxh", "키움히어로즈", "키움", "#830125", "#A7A9AC", "ic_team_nxh"),
     LGT("lgt", "LG트윈스", "LG", "#BD0636", "#1C1C1A", "ic_team_lgt"),
     HHE("hhe", "한화이글스", "한화", "#F37321", "#3D4142", "ic_team_hhe"),
